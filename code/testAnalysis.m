7df36dc5f290e79e828a9810489ae64bc5ec32f0
markgewhite <markgewhite@gmail.com>
2023-09-21 16:41:48 +0100

    Extended testAnalysis

--- code/testAnalysis.m
+++ code/testAnalysis.m
@@ -1,14 +1,14 @@
 % test the model with a grid search
 
-testID = 2;
+clear;
+
+testIndices = 1:2;
 
 % -- data setup --
 setup.data.class = @DelsysDataset;
 
 % -- model setup --
 setup.model.class = @JumpModel;
-setup.model.args.EncodingType = 'Continuous';
-setup.model.args.ContinuousEncodingArgs.ShowConvergence = true;
 
 % --- evaluation setup ---
 setup.eval.args.CVType = 'KFold';
@@ -16,38 +16,54 @@ setup.eval.args.KFolds = 2;
 setup.eval.args.KFoldRepeats = 5;
 setup.eval.args.InParallel = false;
 
-% first investigation
-name = 'test2';
+% results location
 path = fileparts( which('code/testAnalysis.m') );
 path = [path '/../results/'];
 
-switch testID
+myInvestigation = cell( max(testIndices), 1 );
+for i = testIndices
 
-    case 1
-        parameters = [ "model.args.EncodingType", ...
-                       "data.args.Proportion", ...
-                       "data.args.Instance" ];
-        values = {{'Discrete', 'Continuous'}, ...
-                  0.2:0.2:1.0, ...
-                  1:20 };
-        
-        myInvestigation = ParallelInvestigation( name, path, parameters, values, setup );
-        
-        myInvestigation.run;
-        
-        aggrTrainRMSE = mean( myInvestigation.TrainingResults.Mean.RMSE, 3 );
-        aggrValRMSE = mean( myInvestigation.ValidationResults.Mean.RMSE, 3 );
+    % reset arguments
+    if isfield( setup.model, 'args' )
+        setup.model = rmfield( setup.model, 'args' );
+    end
+    if isfield( setup.data, 'args' )
+        setup.data = rmfield( setup.data, 'args' );
+    end
 
-    case 2
-        parameters = [ "model.args.ContinuousEncodingArgs.NumComponents", ...
-                       "data.args.Instance" ];
-        values = { 1:15, 1:20 };
-        
-        myInvestigation = Investigation( name, path, parameters, values, setup );
-        
-        myInvestigation.run;
-        
-        aggrTrainRMSE = mean( myInvestigation.TrainingResults.Mean.RMSE, 2 );
-        aggrValRMSE = mean( myInvestigation.ValidationResults.Mean.RMSE, 2 );
+    switch i
+    
+        case 1
+            name = 'SamplingTest';
+            setup.model.args.ContinuousEncodingArgs.AlignmentMethod = 'LMTakeoff';
+    
+            parameters = [ "data.args.Proportion", ...
+                           "model.args.EncodingType", ...
+                           "data.args.Instance" ];
+            values = {0.2:0.2:1.0, ...
+                      {'Discrete', 'Continuous'}, ...
+                      1:2 };
+            
+            myInvestigation{i} = Investigation( name, path, parameters, values, setup );
+            
+            myInvestigation{i}.run;
+            
+            aggrTrainRMSE = mean( myInvestigation{i}.TrainingResults.Mean.RMSE, 3 );
+            aggrValRMSE = mean( myInvestigation{i}.ValidationResults.Mean.RMSE, 3 );
+    
+        case 2
+            name = 'ContAlignTest';
+            setup.model.args.EncodingType = 'Continuous';
+    
+            parameters = [ "model.args.ContinuousEncodingArgs.NumComponents", ...
+                           "model.args.ContinuousEncodingArgs.AlignmentMethod"];
+            values = { 1:5, ...
+                       {'XCRandom', 'XCMeanConv', 'LMTakeoff', 'LMLanding' } };
+            
+            myInvestigation{i} = ParallelInvestigation( name, path, parameters, values, setup );
+            
+            myInvestigation{i}.run;
+    
+    end
 
 end

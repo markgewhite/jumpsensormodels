cb3561e16a086700c37a3b3e78fb636f7ed4ff55
markgewhite <markgewhite@gmail.com>
2024-02-17 16:02:18 +0000

    Moved plot dist to utils

--- code/showPredictorDistributions.m
+++ code/showPredictorDistributions.m
@@ -13,14 +13,14 @@ discreteEncoding = DiscreteEncodingStrategy;
 discreteXSmart = discreteEncoding.extractFeatures( smartData );
 discreteXDelsys = discreteEncoding.extractFeatures( delsysData );
 
-figDDistSmart = plotPredictorDistributions( discreteXSmart, ...
-                                            discreteEncoding.Names, ...
-                                            varSelection, ...
-                                            "Smartphone (Discrete)" );
-figDDistDelsys = plotPredictorDistributions( discreteXDelsys, ...
-                                             discreteEncoding.Names, ...
-                                             varSelection, ...
-                                             "Delsys (Discrete)" );
+figDDistSmart = plotDistributions( discreteXSmart, ...
+                                   discreteEncoding.Names, ...
+                                   varSelection, ...
+                                   "Smartphone (Discrete)" );
+figDDistDelsys = plotDistributions( discreteXDelsys, ...
+                                    discreteEncoding.Names, ...
+                                    varSelection, ...
+                                    "Delsys (Discrete)" );
 
 % Continuous encodings
 numComp = max(varSelection);
@@ -32,14 +32,14 @@ contEncodingDelsys = FPCAEncodingStrategy( NumComponents = numComp );
 contEncodingDelsys = contEncodingDelsys.fit( delsysData );
 contXDelsys = contEncodingDelsys.extractFeatures( delsysData );
 
-figCDistSmart = plotPredictorDistributions( contXSmart, ...
-                                            contEncodingSmart.Names, ...
-                                            varSelection, ...
-                                            "Smartphone (Continuous)" );
-figCDistDelsys = plotPredictorDistributions( contXDelsys, ...
-                                             contEncodingDelsys.Names, ...
-                                             varSelection, ...
-                                            "Delsys (Continuous)" );
+figCDistSmart = plotDistributions( contXSmart, ...
+                                   contEncodingSmart.Names, ...
+                                   varSelection, ...
+                                   "Smartphone (Continuous)" );
+figCDistDelsys = plotDistributions( contXDelsys, ...
+                                    contEncodingDelsys.Names, ...
+                                    varSelection, ...
+                                    "Delsys (Continuous)" );
 
 
 %% Variations arising from subsampling
@@ -48,12 +48,12 @@ path = [path '/../results/'];
 
 setup.model.class = @JumpModel;
 setup.model.args.ModelType = 'Linear';
-setup.model.args.ContinuousEncodingArgs.AlignmentMethod = 'XCMeanConv';
+setup.model.args.ContinuousEncodingArgs.AlignmentMethod = 'LMTakeoff';
 setup.model.args.ContinuousEncodingArgs.NumComponents = 10;
 
 eval.CVType = 'KFold';
 eval.KFolds = 2;
-eval.KFoldRepeats = 50;
+eval.KFoldRepeats = 25;
 eval.RandomSeed = 1234;
 eval.InParallel = true;
 args = namedargs2cell( eval );
@@ -67,106 +67,23 @@ setup.data.class = @DelsysDataset;
 contEvalDelsys = ModelEvaluation( 'ContVariationDelsys', path, setup, args{:} );
 
 %% plot the spread in X across the folds
+titleSuffix = ['(Continuous: Alignment = '  ...
+               setup.model.args.ContinuousEncodingArgs.AlignmentMethod ')'];
+
 contXSmartKFold = cellfun( @(mdl) table2array(mdl.Model.Variables), ...
                            contEvalSmart.Models, UniformOutput=false );
 
-figCDistVarSmart = plotPredictorDistributions( contXSmartKFold, ...
-                                               contEncodingSmart.Names, ...
-                                               varSelection, ...
-                                               "Smartphone (Continuous)" );
+figCDistVarSmart = plotDistributions( contXSmartKFold, ...
+                                      contEncodingSmart.Names, ...
+                                      varSelection, ...
+                                      ['Smartphone ' titleSuffix]);
 
 contXDelsysKFold = cellfun( @(mdl) table2array(mdl.Model.Variables), ...
                            contEvalDelsys.Models, UniformOutput=false );
 
-figCDistVarDelsys = plotPredictorDistributions( contXDelsysKFold, ...
-                                                contEncodingSmart.Names, ...
-                                                varSelection, ...
-                                                "Delsys (Continuous)" );
+figCDistVarDelsys = plotDistributions( contXDelsysKFold, ...
+                                       contEncodingDelsys.Names, ...
+                                       varSelection, ...
+                                       ['Delsys ' titleSuffix]);
 
 
-function [fig, ax] = plotPredictorDistributions( X, names, idx, figTitle )
-    % Plot distributions for specified variable indices
-    arguments
-        X           {mustBeA(X, {'double', 'cell'})}
-        names       string
-        idx         double {mustBePositive, mustBeInteger}
-        figTitle    string = ""
-    end
-
-    fig = figure;
-    numPlots = length(idx);
-
-    [rows, cols] = sqdim( numPlots );
-    layout = tiledlayout( rows, cols, TileSpacing= 'compact' );
-
-    ax = cell( numPlots, 1 );
-    for i = 1:numPlots
-        ax{i} = nexttile( layout );
-
-        % generate probability density function
-        if iscell(X)
-            [pX, mu, sigma] = plotPDFSpread( ax{i}, X, idx(i) );
-        else
-            [pY, pX] = kde( X(:,idx(i)) );
-            mu = mean( X(:,idx(i)) );
-            sigma = std( X(:,idx(i)) );
-
-            plot( ax{i}, pX, pY, LineWidth = 1.5 );
-            hold( ax{i}, 'on' );
-        end
-
-        % generate equivalent normal distribution
-        nY = (1/(sigma*sqrt(2*pi)))*exp(-.5*(((pX-mu)/sigma).^2));
-        plot( ax{i}, pX, nY, '--', LineWidth = 1.5, color = 'k' );
-        hold( ax{i}, 'off' );
-
-        % format plot
-        xlabel( ax{i}, names(idx(i)) );
-        ylabel( ax{i}, 'Freq' );
-
-    end
-
-    sgtitle( fig, figTitle );
-
-end
-
-
-function [pNormX, mu, sigma] = plotPDFSpread( ax, X, idx )
-
-    numFits = length(X);
-
-    % set the bounds
-    minX = min(cellfun( @(x) min(x(:,idx)), X ));
-    maxX = max(cellfun( @(x) max(x(:,idx)), X ));
-
-    % extend them
-    extRngX = (maxX-minX)*0.15;
-
-    % initialise normalised scale
-    pNormX = linspace( minX-extRngX, maxX+extRngX, 100 )';
-    pNormY = zeros( 100, numFits );
-
-    % fit PDFs for each set
-    for i = 1:numFits
-        pNormY(:,i) = kde( X{i}(:,idx), EvaluationPoints = pNormX );
-    end
-
-    allX = cat(1, X{:} );
-    mu = mean( allX(:,idx) );
-    sigma = std( allX(:,idx) );
-
-    prc25Y = prctile(pNormY, 25, 2);
-    prc50Y = prctile(pNormY, 50, 2);
-    prc75Y = prctile(pNormY, 75, 2);
-
-    % draw the shading spread of +/- SD
-    cmap = lines(1);
-    xSpread = [ pNormX; flip(pNormX) ];
-    ySpread = [ prc75Y; flip(prc25Y) ];
-    fill( ax, xSpread, ySpread, cmap, FaceAlpha = 0.5, EdgeColor = 'none' );
-    hold( ax, 'on' );
-
-    plot( ax, pNormX, prc50Y, LineWidth=1.5, Color=cmap );
-
-
-end
---
+++ code/utils/plotDistributions.m
@@ -0,0 +1,87 @@
+function [fig, ax] = plotDistributions( X, names, idx, figTitle )
+    % Plot distributions for specified variable indices
+    arguments
+        X           {mustBeA(X, {'double', 'cell'})}
+        names       string
+        idx         double {mustBePositive, mustBeInteger}
+        figTitle    string = ""
+    end
+
+    numPlots = length(idx);
+
+    fig = figure;
+    fig.Position(3) = numPlots*200 + 100;
+    fig.Position(4) = 200;
+    layout = tiledlayout( 1, numPlots, TileSpacing= 'compact' );
+
+    ax = cell( numPlots, 1 );
+    for i = 1:numPlots
+        ax{i} = nexttile( layout );
+
+        % generate probability density function
+        if iscell(X)
+            [pX, mu, sigma] = plotPDFSpread( ax{i}, X, idx(i) );
+        else
+            [pY, pX] = kde( X(:,idx(i)) );
+            mu = mean( X(:,idx(i)) );
+            sigma = std( X(:,idx(i)) );
+
+            plot( ax{i}, pX, pY, LineWidth = 1.5 );
+            hold( ax{i}, 'on' );
+        end
+
+        % generate equivalent normal distribution
+        nY = (1/(sigma*sqrt(2*pi)))*exp(-.5*(((pX-mu)/sigma).^2));
+        plot( ax{i}, pX, nY, '--', LineWidth = 1.5, color = 'k' );
+        hold( ax{i}, 'off' );
+
+        % format plot
+        xlabel( ax{i}, names(idx(i)) );
+        ylabel( ax{i}, 'Freq' );
+
+    end
+
+    sgtitle( fig, figTitle );
+
+end
+
+
+function [pNormX, mu, sigma] = plotPDFSpread( ax, X, idx )
+
+    numFits = length(X);
+
+    % set the bounds
+    minX = min(cellfun( @(x) min(x(:,idx)), X ));
+    maxX = max(cellfun( @(x) max(x(:,idx)), X ));
+
+    % extend them
+    extRngX = (maxX-minX)*0.15;
+
+    % initialise normalised scale
+    pNormX = linspace( minX-extRngX, maxX+extRngX, 100 )';
+    pNormY = zeros( 100, numFits );
+
+    % fit PDFs for each set
+    for i = 1:numFits
+        pNormY(:,i) = kde( X{i}(:,idx), EvaluationPoints = pNormX );
+    end
+
+    allX = cat(1, X{:} );
+    mu = mean( allX(:,idx) );
+    sigma = std( allX(:,idx) );
+
+    prc25Y = prctile(pNormY, 25, 2);
+    prc50Y = prctile(pNormY, 50, 2);
+    prc75Y = prctile(pNormY, 75, 2);
+
+    % draw the shading spread of +/- SD
+    cmap = lines(1);
+    xSpread = [ pNormX; flip(pNormX) ];
+    ySpread = [ prc75Y; flip(prc25Y) ];
+    fill( ax, xSpread, ySpread, cmap, FaceAlpha = 0.5, EdgeColor = 'none' );
+    hold( ax, 'on' );
+
+    plot( ax, pNormX, prc50Y, LineWidth=1.5, Color=cmap );
+
+
+end

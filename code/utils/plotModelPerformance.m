7b63d20a26723b0c3d3e546452cb8e9df373951c
markgewhite <markgewhite@gmail.com>
2024-03-09 19:32:30 +0000

    Revised model performance plot

--- code/utils/plotModelPerformance.m
+++ code/utils/plotModelPerformance.m
@@ -1,4 +1,6 @@
-function fig = plotModelPerformance( thisInvestigation, varName, metric, metricName, set )
+function fig = plotModelPerformance( thisInvestigation, ...
+                                     varName, metric, metricName, ...
+                                     set, yLimits, yTickIncrement )
     % Produce a figure of model performance from results
     arguments
         thisInvestigation   Investigation
@@ -6,6 +8,8 @@ function fig = plotModelPerformance( thisInvestigation, varName, metric, metricN
         metric              string
         metricName          string
         set                 char {mustBeMember(set, {'Training', 'Validation'})}
+        yLimits             double = []
+        yTickIncrement      double = 0.25
     end
 
     numEncodings = thisInvestigation.SearchDims(1);
@@ -17,7 +21,7 @@ function fig = plotModelPerformance( thisInvestigation, varName, metric, metricN
     fontname( fig, 'Arial' );
     fig.Position(3) = 350*numEncodings + 100;
     fig.Position(4) = 250*numDatasets + 100;
-    layout = tiledlayout( numDatasets, numEncodings, TileSpacing='loose' );
+    layout = tiledlayout( numDatasets, numEncodings, TileSpacing='compact' );
     colours = lines(numModelTypes);
     
     x0 = thisInvestigation.GridSearch{2};
@@ -30,8 +34,10 @@ function fig = plotModelPerformance( thisInvestigation, varName, metric, metricN
     
     for i = 1:numDatasets
         for j = 1:numEncodings
+
             ax = nexttile(layout);
             hold( ax, 'on' );
+
             for k = 1:numModelTypes
                 x = x0+rand(1,numPredictors)*0.2;
                 plot( ax, x, y(j, :, i, k), ...
@@ -45,14 +51,27 @@ function fig = plotModelPerformance( thisInvestigation, varName, metric, metricN
                     CapSize = 5, ...
                     HandleVisibility = 'off');
             end
+
+            xlim( ax, [0 max(x0)] );
+            if ~isempty(yLimits)
+                ylim(ax, yLimits);
+                if ~isempty(yTickIncrement)
+                    yTicks = yLimits(1):yTickIncrement:yLimits(2);
+                    yTickLabels = arrayfun(@(x) num2str(x, '%.2f'), yTicks, 'UniformOutput', false);
+                    ax.YTick = yTicks;
+                    ax.YTickLabel = yTickLabels;
+                end
+            end
             xlabel( ax, varName );
             ylabel( ax, metricName );
-            legend( ax, modelTypeNames, Location = 'best' );
             heading = strcat( datasetNames(i), " - ", encodingNames(j) );
             title( ax, heading );
+
+            if i==1 && j==1
+                legend( ax, modelTypeNames, Location = 'best' );
+            end
+
         end
     end
-
-    sgtitle( fig, [set ' Set']);
 
 end
